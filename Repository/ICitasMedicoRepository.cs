ef7d0f76a75264db49b3d8a0741726aa478af135
Ginés de Gea <gines@example.com>
2024-03-11 18:06:26 +0100

    Repository añadido

--- Repository/ICitasMedicoRepository.cs
+++ Repository/ICitasMedicoRepository.cs
@@ -1,7 +1,30 @@
-﻿namespace CitasMedico.Repository
+﻿using CitasMedico.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitasMedico.Repository
 {
-    public interface ICitasMedicoRepository where T : class
+    public interface ICitasMedicoRepository<T> where T : class
     {
-       
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+
+    public class CitasMedicoRepository<T> : ICitasMedicoRepository<T> where T : class
+    {
+        private readonly DataContext _context;
+
+        public CitasMedicoRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+        public async Task UpdateAsync(T entity) => await Task.Run(() => _context.Set<T>().Update(entity));
+        public async Task DeleteAsync(T entity) => await Task.Run(() => _context.Set<T>().Remove(entity));
     }
 }
---
+++ Repository/IUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using CitasMedico.Data;
+using CitasMedico.Models;
+
+namespace CitasMedico.Repository
+{
+    public interface IUnitOfWork
+    {
+        ICitasMedicoRepository<Medico> Medicos { get; }
+        ICitasMedicoRepository<Cita> Citas { get; }
+        ICitasMedicoRepository<Diagnostico> Diagnosticos { get; }
+        ICitasMedicoRepository<Paciente> Pacientes { get; }
+        ICitasMedicoRepository<Usuario> Usuarios { get; }
+        Task<int> SaveChangesAsync();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+            Medicos = new CitasMedicoRepository<Medico>(_context);
+            Diagnosticos = new CitasMedicoRepository<Diagnostico>(_context);
+            Pacientes = new CitasMedicoRepository<Paciente>(_context);
+            Usuarios = new CitasMedicoRepository<Usuario>(_context);
+        }
+
+        public ICitasMedicoRepository<Medico> Medicos { get; }
+        public ICitasMedicoRepository<Cita> Citas { get; }
+        public ICitasMedicoRepository<Diagnostico> Diagnosticos { get; }
+        public ICitasMedicoRepository<Paciente> Pacientes{ get; }
+        public ICitasMedicoRepository<Usuario> Usuarios { get; }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+    }
+}
